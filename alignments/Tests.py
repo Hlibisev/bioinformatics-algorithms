dd9331efb5bce09684da503ba9e89df1991a45e7
Hlibisev <hlibisev@gmail.com>
2020-10-06 08:13:18 +0300

    new tests

--- alignments/Tests.py
+++ alignments/Tests.py
@@ -4,12 +4,12 @@ import pandas as pd
 
 
 if __name__ == "__main__":
-    protein1 = "GGGACTGAG"
-    protein2 = "GACT"
+    protein1 = "AAGACTGAG"
+    protein2 = "GAG"
 
     matrix = pd.read_csv("/Users/hlibisev/Documents/GitHub/bioinformatics-algorithms/alignments/BLOSUM")
     print("Локальное выравнивание с BLOSUM: ", local_alignment(protein1, protein2, -4, matrix))
-    # Вывод: Локальное выравнивание с BLOSUM:  ('GGGACTGAG_', '__GACT____')
+    # Вывод: Локальное выравнивание с BLOSUM:  ('AAGACTGAG', '______GAG')
 
     print("Глобальное выравнивание с BLOSUM: ", global_alignment(protein1, protein2, -4, matrix))
-    # Вывод: Глобальное выравнивание с BLOSUM:  ('GGGACTGAG', 'G__ACT___')
+    # Вывод: Глобальное выравнивание с BLOSUM:  ('AAGACTGAG', '__GA__G__')
